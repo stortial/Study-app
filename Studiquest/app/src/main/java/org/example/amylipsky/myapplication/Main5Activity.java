770dc49487ea4cd6e262b87b3e82a0343ad98c48
stortial <adstorti@gmail.com>
2017-10-20 14:28:00 -0400

    add comments
    comment activity 5

--- Studiquest/app/src/main/java/org/example/amylipsky/myapplication/Main5Activity.java
+++ Studiquest/app/src/main/java/org/example/amylipsky/myapplication/Main5Activity.java
@@ -3,33 +3,23 @@ package org.example.amylipsky.myapplication;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
-
-import java.security.acl.Group;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Random;
 
 //Google Maps Class
 public class Main5Activity extends AppCompatActivity implements OnMapReadyCallback {
-
 
     private GoogleMap mMap;
     private String val;
@@ -49,17 +39,13 @@ public class Main5Activity extends AppCompatActivity implements OnMapReadyCallba
 
     private double longitude = 0;
     private double lat = 0;
-    ArrayList<String> locationkey = new ArrayList<String>();
-    //ArrayList<String> locationlist = new ArrayList<String>();
     private static ArrayList<String> location_list = new ArrayList<String>();
     private static ArrayList<String> courselist = new ArrayList<String>();
     private static ArrayList<String> startlist = new ArrayList<String>();
     private static ArrayList<String> endlist = new ArrayList<String>();
     private static ArrayList<String> ppllist = new ArrayList<String>();
-    private Handler UI_Handler = new Handler();
 
-    private static final String TAG = "DataBase";
-
+    //creates the map if it hasnt been initialized already
     private void mapSetup(){
         if (mMap == null){
             // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -70,7 +56,7 @@ public class Main5Activity extends AppCompatActivity implements OnMapReadyCallba
         }
     }
 
-
+    //creates the bounds for both the center of the map and the bounds
     private LatLngBounds UniversityAtBuffalo = new LatLngBounds(new LatLng(42.99262, -78.799561),
             new LatLng(43.012405, -78.770156));
 
@@ -79,24 +65,26 @@ public class Main5Activity extends AppCompatActivity implements OnMapReadyCallba
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
 
+        //gets the data from the database
+        //  note, this happens after the first time the map is created
+        //  to fix this just back out and then go back in
         FirebaseDatabase.getInstance().getReference().child("groups").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-
+                //iterate through all children of groups
                 for(DataSnapshot snapshot: dataSnapshot.getChildren()){
 
                     System.out.println("MELL"+snapshot.getKey());
 
-
+                    //put each piece of data into the appropriate variable
                     String course = (String) snapshot.child("course").getValue();
                     String endtime = (String) snapshot.child("endtime").getValue();
                     String locations = (String) snapshot.child("locations").getValue();
                     String numppl = (String) snapshot.child("numppl").getValue();
                     String starttime = (String) snapshot.child("starttime").getValue();
 
-                    //Group mgroup = snapshot.getValue(Group.class);
-
+                    //add each piece of data to the array list
                     location_list.add(locations);
                     courselist.add(course);
                     startlist.add(starttime);
@@ -127,24 +115,18 @@ public class Main5Activity extends AppCompatActivity implements OnMapReadyCallba
 
         //mMap.setLatLngBoundsForCameraTarget(UniversityAtBuffalo);
 
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(UniversityAtBuffalo, 17));
-
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UniversityAtBuffalo.getCenter(), 17));
 
-        //LatLng test = new LatLng(43.001268, -78.789202);
 
-        //Marker tester = mMap.addMarker(new MarkerOptions().position(test).title(" ").snippet("Capen CSE 3:00pm 6:00pm 5"));
-
-        // Add a marker in Sydney and move the camera
         LatLng buffalo = new LatLng(43, -78.7865);
         //mMap.addMarker(new MarkerOptions().position(buffalo).title("Marker in Buffalo"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(buffalo));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(buffalo, 14));
 
-        //UI_Handler.postDelayed(UI_UPDATE_RUNNABLE, 5000);
-
         Random rand = new Random();
 
+
+        //clear all the markers from the map
         //mMap.clear();
 
         for(int i = 0; i<location_list.size(); ++i) {
@@ -165,6 +147,7 @@ public class Main5Activity extends AppCompatActivity implements OnMapReadyCallba
                 lat = 43.000867 + (43.001451 - 43.000867) * rand.nextDouble();
             }
 
+            //set the temp lat and lng for the current marker
             LatLng temp = new LatLng(lat, longitude);
 
             System.out.println("AdAM"+lat+" "+longitude);
@@ -175,72 +158,15 @@ public class Main5Activity extends AppCompatActivity implements OnMapReadyCallba
 
         }
 
-        /**/
-    }
-/*
-    @Override
-    public void onPause(){
-        super.onPause();
-
-
-
     }
 
-/**/
-
+    //recreate the map, but this is done with the static array lists
     @Override
     public void onResume(){
         super.onResume();
         mapSetup();
-
-
-
     }
 
-/*
-    Runnable UI_UPDATE_RUNNABLE = new Runnable() {
-
-        @Override
-        public void run() {
-
-            theMarkers.clear();
-
-            Random rand = new Random();
-
-            System.out.println(location_list.size());
-
-
-            for(int i = 0; i<location_list.size(); ++i) {
-
-                System.out.println(location_list.get(i));
-
-                if (location_list.get(i) == "Capen") {
-                    longitude = -78.789966 + (-78.789202 + 78.789966) * rand.nextDouble();
-                    lat = 43.000523 + (43.001268 - 43.000523) * rand.nextDouble();
-                } else if (location_list.get(i)== "Lockwood") {
-                    longitude = -78.786336 + (-78.785688 + 78.786336) * rand.nextDouble();
-                    lat = 42.999886 + (43.000597 - 42.999886) * rand.nextDouble();
-                } else if (location_list.get(i)== "SU") {
-                    longitude = -78.786780 + (-78.785832 + 78.786780) * rand.nextDouble();
-                    lat = 43.000867 + (43.001451 - 43.000867) * rand.nextDouble();
-                }
-
-                LatLng temp = new LatLng(lat, longitude);
-
-                //here is where i add the actual stuff
-                Marker test = mMap.addMarker(new MarkerOptions().position(temp).title(" ").snippet(
-                        getLocation+" "+getCourse+" "+getStart+" "+getEnd+" "+ getnumppl));
-
-                theMarkers.add(test);
-
-                }
-
-            UI_Handler.postDelayed(UI_UPDATE_RUNNABLE, 30000);
-
-        };
-
-
-    };*/
 }
 
 
